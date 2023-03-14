4aaffbfe145827afa1a2b007adba464bdac1c3e1
senyuanzi <senyuanzi@outlook.com>
2023-03-14 15:25:45 +0800

    release 1.1.0

---
+++ index.ts
@@ -0,0 +1,53 @@
+
+import { createPinia, type PiniaPluginContext } from "pinia";
+import { useRoute, useRouter } from "vue-router";
+
+declare module 'pinia' {
+    export interface PiniaCustomProperties {
+        init: () => void
+    }
+
+    export interface DefineStoreOptionsBase<S, Store> {
+        lifecycle?: string[] | string
+    }
+}
+
+export const piniaLifecycle = ({ options, store }: PiniaPluginContext) => {
+    const initEntries = Object.entries(store.$state);
+
+    const route = useRoute()
+    const initPath = route.path;
+    const modules: string[] = []
+
+    function checkPath(path: string) {
+        return modules.some((module) => path == module || path == '/' + module || path.startsWith(module + '/') || path.startsWith('/' + module + '/'))
+    }
+
+    if (Array.isArray(options.lifecycle)) {
+        modules.push(...options.lifecycle)
+    }
+
+    if (options.lifecycle == 'module') {
+        modules.push(initPath)
+    }
+
+    if (modules.length > 0) {
+        const router = useRouter()
+        router.beforeEach((to, from) => {
+
+            if (!checkPath(to.path)) {
+                initEntries.forEach((entry) => {
+                    store[entry[0]] = entry[1];
+                });
+            }
+        })
+    }
+
+    return {
+        init() {
+            initEntries.forEach((entry) => {
+                store[entry[0]] = entry[1];
+            });
+        },
+    };
+}
